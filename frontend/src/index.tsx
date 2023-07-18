949dbcb2604862d2c1cad550fb3dba0f1c1588be
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-07-18 22:34:41 +0300

    Switched to ts

---
+++ frontend/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './scss/index.scss';
+import App from './components/App';
+
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
